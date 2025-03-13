055871b37ecaf36fc621a211bae5318b4a668333
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2025-03-13 16:10:47 +0900

    교수님의 답
    시험문제 답안지에 쓸 때 이렇게 해라

--- stl/STL화목/메인.cpp
+++ stl/STL화목/메인.cpp
@@ -7,32 +7,42 @@
 #include<iostream>
 #include <random>
 #include<print>
+#include<vector>
+#include<array>
+#include<algorithm>
 #include"save.h"
 
 using namespace std;
 
-default_random_engine dre; //엔전에서 막 임의의 bit를 가공한다
-uniform_int_distribution uid{ 1,999'9999 };
+default_random_engine dre{  }; //엔진의 시드를 설정할 수 있다. 
+uniform_int_distribution uid{ 0,999'9999 }; 
+
+
 //[문제] 랜덤 int 1000개를 메모리에 저장하라
-// 가장 큰 값을 찾아 화면에 출력하라
+// 저장된 값 중에서 가장 큰 값을 찾아 화면에 출력하라 
+
 void find_max_num(int num[]);
 
 //----------
 int main()
 //----------
 {
-	int p[1000];
-	for (int i = 0; i < 1000; ++i) {
-		p[i] = uid(dre);
-		print("{:8}", p[i]);  //나는 8 글자에 맞추어 볼래
+	array<int, 1000> arr; //일관된 표현식으로 바뀐다.
+
+	for (int& num : arr) { //지역에서 지역밖에 있는거 못함 
+		num = uid(dre);
 	}
-	cout << endl;
-	find_max_num(p);
+
+	//end는 iterator 가 마지막 원소 뒤에 값을 가리킨다.
+
+	cout << " 가장 큰 수는 :" << *max_element(arr.begin(), arr.end()) << endl;
+	
 
 	save("메인.cpp");	
 }
 void find_max_num(int num[]) {
-	int maxNum = 0;
+	int maxNum{ numeric_limits<int>::min() }; //공부 좀 한 학생은 이렇게 쓴다.
+
 	for (int i = 0; i < 1000; ++i) {
 		maxNum = max(maxNum, num[i]);
 	}
