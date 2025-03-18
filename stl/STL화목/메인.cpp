5870abf96f03b8bdfc085726fc3aa83f0fe8550d
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2025-03-18 13:52:26 +0900

    16,10 2 진수

--- stl/STL화목/메인.cpp
+++ stl/STL화목/메인.cpp
@@ -5,22 +5,16 @@
 //
 //-------------------------------------------------------------------------------------------
 #include<iostream>
-#include<random>
-#include<print>
-#include<fstream>
-#include<array>
+#include<bitset>
 #include<algorithm>
 #include"save.h"
 
 using namespace std;
 
-default_random_engine dre{  }; //엔진의 시드를 설정할 수 있다. 
-uniform_int_distribution uid{ 0,999'9999 }; 
-
 
 //[문제] 파일 "int 100개.txt"에는 int 1000개가 text로 기록되어 있다.
-// 이 데이터를 메모리에 저장하라.
-// 메모리에 있는 데이터를 화면에 출력하시오.
+//  int 1000개를 저장하기 위해 몇 바이트를 사용했는가?? 8200
+
 
 
 
@@ -28,19 +22,12 @@ uniform_int_distribution uid{ 0,999'9999 };
 int main()
 //----------
 {
-   
-    ifstream in{ "int 1000개.txt" };
-    if (not in)
-        return 20250313;
-    
-    array<int, 1000> arr;
-    for (int i = 0; i < 1000; ++i) {
-        in >> arr[i];
-    }
-    for (auto num : arr) {
-        print("{:8}", num);
-    }
-    cout << endl;
+	int num{ 0x01'02'03'04 };
+	bitset<32>binary = num;
+	cout << " 2진수 -" << binary << endl;
+	cout << "10진수 -" << num << endl;
+	cout << "16진수 -" << hex << num << endl;
+	
+	
 	save("메인.cpp");	
-
 }
