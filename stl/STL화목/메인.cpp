ee2cde7a8ed4c85b9c35ba030427be79cc662ada
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2025-03-18 14:50:34 +0900

    //binary로 열어 기록한 파일 "int 10만개에를 바이너리모드 wirte 함수로 기록" 에는, //int값 10만개가 메모리 크기 그대로 기록되어 있다. (ostream의 wirte 함수를 사용) // int값 10만개를 읽어서 메모리에 저장하시오. // 가장 작은 값을 찾아 화면에 출력하라. // 화면에 출력된 값도 답지에 적어라.
    내가 푼 답

--- stl/STL화목/메인.cpp
+++ stl/STL화목/메인.cpp
@@ -8,31 +8,37 @@
 //-------------------------------------------------------------------------------------------
 #include<iostream>
 #include<fstream>
-#include<random>
+#include<array>
 #include"save.h"
 
 using namespace std;
 
-//파일 "int 10만개에를 바이너리모드 wirte 함수로 기록" 에 10만개의 random int 값을 기록하자.
-//이 int값은 메모리 크기 그대로 기록하겠다.
-//파일은 binary mode로 열자.
+//binary로 열어 기록한 파일 "int 10만개에를 바이너리모드 wirte 함수로 기록" 에는,
+//int값 10만개가 메모리 크기 그대로 기록되어 있다. (ostream의 wirte 함수를 사용)
+// int값 10만개를 읽어서 메모리에 저장하시오.
+// 가장 작은 값을 찾아 화면에 출력하라.
+// 화면에 출력된 값도 답지에 적어라.
 
 
-default_random_engine dre;
+
 
 
 //----------
 int main()
 //----------
 {
-	ofstream out{ "int 10만개에를 바이너리모드 wirte 함수로 기록", ios::binary };
-
-	uniform_int_distribution uid{ numeric_limits<int>::min(),numeric_limits<int>::max() }; //int의 최소부터 최댓값까지
-
-	int num; //이게 훨씬 좋은 방법
-	for (int i = 0; i < 10'0000; ++i) {
-		num = uid(dre);
-		out.write((char *)&num, sizeof(int)); //바이트 단위만 다룸으로 바이트 주소임을 알려줌
+	array<int, 100000> p;
+	ifstream in{ "int 10만개에를 바이너리모드 wirte 함수로 기록",ios::binary };
+	if (!in) {
+		cerr << "파일을 열 수 없습니다." << endl;
+		return 1;
 	}
+	in.read((char*)(p.data()),  sizeof(int)*p.size() );
+	if (!in) {
+		cerr << "파일을 열 수 없습니다." << endl;
+		return 1;
+	}
+
+	cout << "가장 작은 수 :" << *min_element(p.begin(), p.end()) << endl;
 	save("메인.cpp");	
 }
