109e4914543e8e6cef61de0179755c33b1244cfc
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2025-03-18 14:37:29 +0900

    //파일 "int 10만개에를 바이너리모드 wirte 함수로 기록" 에 10만개의 random int 값을 기록하자. //이 int값은 메모리 크기 그대로 기록하겠다. //파일은 binary mode로 열자.
    교수님

--- stl/STL화목/메인.cpp
+++ stl/STL화목/메인.cpp
@@ -1,37 +1,38 @@
 //-------------------------------------------------------------------------------------------
 // 2025 STL 화56 목78  3월 13일 목요일      (2주 2일차 강의) 
 //-------------------------------------------------------------------------------------------
-//   많은 수의 자료를 처리하기
-//
+//   많은 수의 자료를 처리하기 -binary I/O
+//          ->파일을 binary 모드로 열어야 변환이 일어나지 않는다.
 //-------------------------------------------------------------------------------------------
 // 
 //-------------------------------------------------------------------------------------------
 #include<iostream>
-#include<array>
-#include<numeric>
 #include<fstream>
+#include<random>
 #include"save.h"
 
 using namespace std;
 
+//파일 "int 10만개에를 바이너리모드 wirte 함수로 기록" 에 10만개의 random int 값을 기록하자.
+//이 int값은 메모리 크기 그대로 기록하겠다.
+//파일은 binary mode로 열자.
 
 
-
-
+default_random_engine dre;
 
 
 //----------
 int main()
 //----------
 {
-	array<int, 1000> a;
-	iota(a.begin(), a.end(), 1); //a의 시작부터 b의 끝까지 1부터 1씩 증가시켜서 넣어줘
+	ofstream out{ "int 10만개에를 바이너리모드 wirte 함수로 기록", ios::binary };
 
-	cout << "a에 들어있는 int 개수 -" << a.size() << endl;
-	//[문제] a의 data를 파일 "int 1000개 메모리 그대로" 에 기록하자
-	
-	ofstream out{ "int 1000개 메모리 그대로" ,ios::binary };  //파일을 텍스트 모드로 열었다
-	out.write((char*)a.data(), sizeof(int) * a.size()); //이파일에 메모리 그대로 입력하겠어 a.data() 어디다 저장했는지 알려준다. int가 들어있는 곳을 알려줌
-	
+	uniform_int_distribution uid{ numeric_limits<int>::min(),numeric_limits<int>::max() }; //int의 최소부터 최댓값까지
+
+	int num; //이게 훨씬 좋은 방법
+	for (int i = 0; i < 10'0000; ++i) {
+		num = uid(dre);
+		out.write((char *)&num, sizeof(int)); //바이트 단위만 다룸으로 바이트 주소임을 알려줌
+	}
 	save("메인.cpp");	
 }
