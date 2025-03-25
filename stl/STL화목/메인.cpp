937c460770dbd32ab8bb79e1fb86c996ec51d427
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2025-03-25 13:51:08 +0900

    문제
    //[문제] int의 최댓값까지  양의 정수 num을 입력받는다.
    // 1부터  num 까지의 정수를 저장할 수 있는 메모리를 확보하고
    // 메모리의 값을 1부터 num까지 채운다.
    // 메모리에 저장된 값의 합계를화면에 출력한다.
    // 화면에 출력된 합계는 답지에도 써라.
    // 이 문제가 아무런 문제 없이 영원히 반복실행될 수 있게 코딩하라

--- stl/STL화목/메인.cpp
+++ stl/STL화목/메인.cpp
@@ -1,64 +1,50 @@
 //-------------------------------------------------------------------------------------------
 // 2025 STL 화56 목78  3월 20일 목요일      (3주 2일차 강의) 
 //-------------------------------------------------------------------------------------------
-//   많은 수의 자료를 처리하기 - Dog를 읽고 쓴다 , read/wirte
-// 
-//  이때 Dog 객체는 메모리 영역
-// 
-//  1.STACK
-//	2.DATA
-//  3.free-store
-// 
-// 1,2 번과 3번의 차이 -  크기가 언제 결정할 수 있는 가?
-// 1번과 2번의 차이.   -  메모리 크기가 얼마나 큰가?
-// 
-//  다음 시간 이거 조금 더 설명하고 smart pointer로 직행 - 왜 RAII?
+// 동적할당과 smart pointer
 //-------------------------------------------------------------------------------------------
 // constexpr 가 뭘까??? 알아보자
 // structured-binding
 //-------------------------------------------------------------------------------------------
 #include<iostream>
-#include<fstream>
+#include<vector>
 #include<array>
 #include"save.h"
 
 
 using namespace std;
 
-// [문제] eclass에서 다운 받은 파일 "Dog 10만마리" 는 binary mode로 열었고,
-// 위에 적은 class Dog를 스트림의 write 함수를 사용하여 메모리 그대로 저장하였다.
-
-
-
-class Dog {
-private:
-	string name;
-	int id{};
-
-
-	friend ostream& operator<<(ostream& os, const Dog& dog) { //friend는 클래스에서 제일 밑바닥에 있어야 한다.
-		return os << "개 이름: " << dog.name << ",개 ID: " << dog.id;
-	}
-
-	friend ifstream& operator>>(ifstream& in,  Dog& dog) {
-		in.read((char*)&dog, sizeof(Dog));
-		return in;
-	}
-};
-
-array<Dog, 10'0000> dogs;
 //----------
 int main()
 //----------
 {
-	ifstream in{ "Dog 10만마리", ios::binary };
-	if (not in)
-		return 1231412;
-	// [문제] 다음 코드가 수정없이 실행되도록 필요한 코드를 class Dog에 추가하라.
-	for (Dog& dog : dogs) {
-		in >> dog;  
-		cout << dog << endl;
-	}
+	//[문제] 양의 정수 num을 입력받는다.
+	// 1부터  num 까지의 정수를 저장할 수 있는 메모리를 확보하고 
+	// 메모리의 값을 1부터 num까지 채운다.
+	// 메모리에 저장된 값의 합계를화면에 출력한다.
+	// 화면에 출력된 합계는 답지에도 써라.
+	// 이 문제가 아무런 문제 없이 영원히 반복실행될 수 있게 코딩하라
 
+	while(true) {
+		cout << " 정수를 입력하세요: ";
+		size_t num;
+		cin >> num;
+		
+		int* arr = new int[num];
+
+		// 배열을 1부터 num까지 채우기
+		for (size_t i = 0; i < num; ++i) {
+			arr[i] = i + 1;
+		}
+		
+		int sum = 0;
+		for (size_t i = 0; i < num; ++i) {
+			sum += arr[i];
+		}
+
+		cout << "1부터 " << num << " 까지 합계 :" << sum << endl;
+
+		delete[] arr;
+	}
 	save("메인.cpp");	
 }
